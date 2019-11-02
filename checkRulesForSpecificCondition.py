ba243490dc8029c0e3fe148f69623839ffd3b539
mesutgungor <clietech@gmail.com>
2019-11-02 15:00:55 +0300

    First Commit

---
+++ checkRulesForSpecificCondition.py
@@ -0,0 +1,134 @@
+import requests
+import json
+from ipaddress import ip_address
+import re
+import warnings
+
+
+
+base_url = 'https://api.meraki.com/api/v0'
+api_key = 'API_KEY'
+networkid = 'Template network id or firewall network id'
+
+def getmxl3fwrules(apikey, networkid, suppressprint=False):
+    calltype = 'MX L3 Firewall'
+    geturl = '{0}/networks/{1}/l3FirewallRules'.format(str(base_url), str(networkid))
+    headers = {
+        'x-cisco-meraki-api-key': format(str(apikey)),
+        'Content-Type': 'application/json'
+    }
+    dashboard = requests.get(geturl, headers=headers)
+    result = __returnhandler(dashboard.status_code, dashboard.text, calltype, suppressprint)
+    return result
+
+def __isjson(myjson):
+    """
+
+    Args:
+        myjson: String variable to be validated if it is JSON
+
+    Returns: None
+
+    """
+    try:
+        json_object = json.loads(myjson)
+    except ValueError:
+        return False
+    return True
+
+def __returnhandler(statuscode, returntext, objtype, suppressprint):
+    """
+
+    Args:
+        statuscode: HTTP Status Code
+        returntext: JSON String
+        objtype: Type of object that operation was performed on (i.e. SSID, Network, Org, etc)
+        suppressprint: Suppress any print output when function is called
+
+    Returns:
+        errmsg: If returntext JSON contains {'errors'} element
+        returntext: If no error element, returns returntext
+
+    """
+
+    validreturn = __isjson(returntext)
+    noerr = False
+    errmesg = ''
+
+    if validreturn:
+        returntext = json.loads(returntext)
+
+        try:
+            errmesg = returntext['errors']
+        except KeyError:
+            noerr = True
+        except TypeError:
+            noerr = True
+
+    if str(statuscode) == '200' and validreturn:
+        if suppressprint is False:
+            print('{0} Operation Successful - See returned data for results\n'.format(str(objtype)))
+        return returntext
+    elif str(statuscode) == '200':
+        if suppressprint is False:
+            print('{0} Operation Successful\n'.format(str(objtype)))
+        return None
+    elif str(statuscode) == '201' and validreturn:
+        if suppressprint is False:
+            print('{0} Added Successfully - See returned data for results\n'.format(str(objtype)))
+        return returntext
+    elif str(statuscode) == '201':
+        if suppressprint is False:
+            print('{0} Added Successfully\n'.format(str(objtype)))
+        return None
+    elif str(statuscode) == '204' and validreturn:
+        if suppressprint is False:
+            print('{0} Deleted Successfully - See returned data for results\n'.format(str(objtype)))
+        return returntext
+    elif str(statuscode) == '204':
+        print('{0} Deleted Successfully\n'.format(str(objtype)))
+        return None
+    elif str(statuscode) == '400' and validreturn and noerr is False:
+        if suppressprint is False:
+            print('Bad Request - See returned data for error details\n')
+        return errmesg
+    elif str(statuscode) == '400' and validreturn and noerr:
+        if suppressprint is False:
+            print('Bad Request - See returned data for details\n')
+        return returntext
+    elif str(statuscode) == '400':
+        if suppressprint is False:
+            print('Bad Request - No additional error data available\n')
+    elif str(statuscode) == '401' and validreturn and noerr is False:
+        if suppressprint is False:
+            print('Unauthorized Access - See returned data for error details\n')
+        return errmesg
+    elif str(statuscode) == '401' and validreturn:
+        if suppressprint is False:
+            print('Unauthorized Access')
+        return returntext
+    elif str(statuscode) == '404' and validreturn and noerr is False:
+        if suppressprint is False:
+            print('Resource Not Found - See returned data for error details\n')
+        return errmesg
+    elif str(statuscode) == '404' and validreturn:
+        if suppressprint is False:
+            print('Resource Not Found')
+        return returntext
+    elif str(statuscode) == '500':
+        if suppressprint is False:
+            print('HTTP 500 - Server Error')
+        return returntext
+    elif validreturn and noerr is False:
+        if suppressprint is False:
+            print('HTTP Status Code: {0} - See returned data for error details\n'.format(str(statuscode)))
+        return errmesg
+    else:
+        print('HTTP Status Code: {0} - No returned data\n'.format(str(statuscode)))
+
+response = getmxl3fwrules(api_key,networkid)
+
+for item in response: 
+        if('specific vlan or ip address' in item['destCidr']): #Here you can check various conditions that is required for PCI-DSS rule audit 
+                print(item) #Do What you want when the condition is occured you can email the rule
+
